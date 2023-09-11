aebfb24e2ba19e1615e7f125a3a91e8fae6a6767
Felipe Bertoglio <felipe.bertoglio@gmail.com>
2023-09-11 20:08:11 -0300

    upd

--- code/subservices/interface_subservice.cpp
+++ code/subservices/interface_subservice.cpp
@@ -147,46 +147,47 @@ int InterfaceSubservice::updateClientScreen()
 
   while (this->isActive() == true && sessionMode != "manager")
   {
-    // set cursor to top left'
-    string userCommand;
-    getline(cin, userCommand);
-    this->enablePrinting = false;
-    system("clear");
-    this->gotoxy(1, 5);
-    printList(tabelaParticipantes);
-    system("tput ed");
-    this->gotoxy(0, 0);
-    cout << "Insert Command > ";
+    continue;
+    // // set cursor to top left'
+    // string userCommand;
+    // getline(cin, userCommand);
+    // this->enablePrinting = false;
+    // system("clear");
+    // this->gotoxy(1, 5);
+    // printList(tabelaParticipantes);
+    // system("tput ed");
+    // this->gotoxy(0, 0);
+    // cout << "Insert Command > ";
 
-    getline(cin, userCommand);
-    string command = userCommand.substr(0, userCommand.find(" "));
-    string argument = userCommand.substr(userCommand.find(" ") + 1);
-    if (command == "HELP")
-    {
-      this->enablePrinting = false;
-      system("clear");
-      cout << "  -  HELP - show this help" << endl;
-      cout << "  -  EXIT - exit the program" << endl;
-      cout << "  -  Current manager: " << MANAGER_IP_ADDRESS << endl;
-      cout << "  -  Current session mode: " << sessionMode << endl;
-      cout << "  -  Monitoring status: " << monitoring_status << endl;
-      cout << "  -  Replica status: " << replica_status << endl;
-      cout << "  -  Discovery status: " << discovery_status << endl;
-      cout << "  -  Interface status: " << interface_status << endl;
-      cout << endl
-           << "Press anything to quit help tab" << endl;
-      cin.get();
-      system("clear");
-    }
-    else if (command == "EXIT")
-    {
-      exit(0);
-    }
-    this->enablePrinting = true;
-    gotoxy(0, 0);
-    cout << "Press Enter to insert command";
-    this->gotoxy(1, 5);
-    system("tput ed");
+    // getline(cin, userCommand);
+    // string command = userCommand.substr(0, userCommand.find(" "));
+    // string argument = userCommand.substr(userCommand.find(" ") + 1);
+    // if (command == "HELP")
+    // {
+    //   this->enablePrinting = false;
+    //   system("clear");
+    //   cout << "  -  HELP - show this help" << endl;
+    //   cout << "  -  EXIT - exit the program" << endl;
+    //   cout << "  -  Current manager: " << MANAGER_IP_ADDRESS << endl;
+    //   cout << "  -  Current session mode: " << sessionMode << endl;
+    //   cout << "  -  Monitoring status: " << monitoring_status << endl;
+    //   cout << "  -  Replica status: " << replica_status << endl;
+    //   cout << "  -  Discovery status: " << discovery_status << endl;
+    //   cout << "  -  Interface status: " << interface_status << endl;
+    //   cout << endl
+    //        << "Press anything to quit help tab" << endl;
+    //   cin.get();
+    //   system("clear");
+    // }
+    // else if (command == "EXIT")
+    // {
+    //   exit(0);
+    // }
+    // this->enablePrinting = true;
+    // gotoxy(0, 0);
+    // cout << "Press Enter to insert command";
+    // this->gotoxy(1, 5);
+    // system("tput ed");
   }
   printTable_thr.join();
   cout << "return interface subservice" << endl;
